b55c131c34200822ddc2921bf0e27b2f804cbc69
Kanwaljeet Singh Kalra <4933214+KKS1@users.noreply.github.com>
2025-11-28 11:51:20 -0600

    websocket endpoint

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 from typing import List
 from uuid import uuid4
 from dotenv import load_dotenv
+from fastapi.encoders import jsonable_encoder
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
 from langgraph.graph import StateGraph
 from langgraph.checkpoint.memory import MemorySaver
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 load_dotenv()
 memory = MemorySaver()
@@ -49,12 +50,14 @@ app = builder.compile(checkpointer=memory)
 # --------------------------------------
 api = FastAPI(title="LangGraph Chat API", description="LangGraph Chat API", version="0.1.0")
 
+# input/output models
 class UserInput(BaseModel):
     content: str = "Tell me a joke"
 
 class Result(State):
     thread_id: str
 
+# Post API endpoint for chat interactions
 @api.post("/chat")
 async def chat(user_input: UserInput, thread_id: str = None) -> Result:
     if thread_id is None:
@@ -66,6 +69,42 @@ async def chat(user_input: UserInput, thread_id: str = None) -> Result:
     state = add_message(state, "user", user_input.content)
     result = await app.ainvoke(state.model_dump(), config={"configurable": {"thread_id": thread_id}})
     return Result(**result, thread_id=thread_id) 
+
+# WebSocket endpoint for real-time chat
+@api.websocket("/ws/{thread_id}")
+async def websocket_endpoint(websocket: WebSocket, thread_id: str):
+    await websocket.accept()
+    while True:
+        try:
+            data = await websocket.receive_json()
+            user_input = UserInput(**data)
+            saved = memory.get({"configurable": {"thread_id": thread_id}})
+            prev = []
+
+            if saved and "channel_values" in saved and "messages" in saved["channel_values"]:
+                prev = [ msg.model_dump() for msg in saved["channel_values"]["messages"]]
+
+            state = State(messages=prev)
+            state = add_message(state, "user", user_input.content)
+
+            # Stream assistant response
+            async for event in app.astream(state.model_dump(), config={"configurable": {"thread_id": thread_id}}):
+                safe_event = jsonable_encoder(event)
+                safe_event["thread_id"] = thread_id
+
+                await websocket.send_json(safe_event)
+            
+            # Send done message (optional)
+            await websocket.send_json({"thread_id": thread_id, "type": "done"})
+
+        except WebSocketDisconnect:
+            print(f"WebSocket disconnected: {thread_id}")
+            break
+        except Exception as e:
+            await websocket.send_json({"thread_id": thread_id, "type": "error", "message": str(e)})
+
+
+
 
 # --------------------------------------
 # 5. Run with uvicorn
